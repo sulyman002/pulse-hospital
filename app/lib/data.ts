699b1d2b2697d46477ef9d2c49941de550e6bf6c
SulymanTheDev <oyedelesulaiman@gmail.com>
2026-01-29 12:50:34 +0000

    i added layout for the onboarding

---
+++ app/lib/data.ts
@@ -0,0 +1,5 @@
+  export const tabs = [
+    { name: 'Primary information', path: '/setup/primary-information' },
+    { name: 'Services', path: '/setup/services' },
+    { name: 'Locations', path: '/setup/locations' },
+  ]
---
+++ app/setup/layout.tsx
@@ -0,0 +1,68 @@
+"use client";
+
+import { usePathname, useRouter } from "next/navigation";
+import { tabs } from "../lib/data";
+
+const SetupLayout = ({ children }: { children: React.ReactNode }) => {
+  const pathname = usePathname();
+  const router = useRouter();
+  return (
+    <main className="bg-gray-50 min-h-screen p-8">
+      <div className="mx-auto max-w-6xl ">
+        {/* Header */}
+        <div className="mb-6">
+          <div className="flex items-center gap-3 mb-2">
+            <div className="w-10 h-10 bg-blue-600 rounded flex items-center justify-center text-white">
+              H
+            </div>
+            <div>
+              <h1 className="text-xl secondary-text font-700 font-bold">
+                Setup Hospital
+              </h1>
+              <p className="text-sm text-[#8E919C]">Supply some information</p>
+            </div>
+          </div>
+        </div>
+
+        {/* Main Container */}
+    <div className="bg-white rounded-lg border border-[#DFE8FC] p-8">
+          {/* Tab Navigation */}
+          <div className="flex border-b border-[#E6E8EC] ">
+            <div className="gap-8 mb-8 flex flex-1">
+                {tabs.map((tab) => {
+              const isActive = pathname === tab.path;
+              return (
+                <button
+                  key={tab.path}
+                  onClick={() => router.push(tab.path)}
+                  className={`
+                    pb-4 px-2 text-sm font-medium transition-colors relative
+                    ${
+                      isActive
+                        ? "primary-text text-base"
+                        : "text-[#8E919C] hover:text-gray-700"
+                    }
+                  `}
+                >
+                  {tab.name}
+                  {isActive && (
+                    <div className="absolute bottom-0 left-0 right-0 h-0.5 primary-bg" />
+                  )}
+                </button>
+              );
+            })}
+            </div>
+            <button className="text-base font-700 text-semibold rounded-lg px-4 py-2 text-white tertiary-bg ">Next</button>
+          </div>
+
+          {/* Tab Content */}
+          {children}
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default SetupLayout;
+
+
